17d00b9fd7ddb800f2e76c5248a1ebeb03154be1
uluumbch <uluum123@gmail.com>
2022-03-18 03:59:37 +0800

    merapikan kode dan menambahkan form handling

--- modul2/PRAK202.php
+++ modul2/PRAK202.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bachrul Uluum</title>
+    <title>Soal 2 - Bachrul Uluum</title>
     <style>
         .error {
             color: #FF0000;
--- modul2/PRAK203.php
+++ modul2/PRAK203.php
@@ -5,18 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Konversi Suhu</title>
+    <title>Soal 3 - Konversi Suhu</title>
 </head>
 
 <body>
     <?php
-    $suhu = $_POST['nilai'];
-    $dariSuhu = $_POST['dariSuhu'];
-    $keSuhu = $_POST['keSuhu'];
-    $hasil;
+    if (isset($_POST['enter'])) {
+
+        $suhu = $_POST['nilai'];
+        $dariSuhu = $_POST['dariSuhu'];
+        $keSuhu = $_POST['keSuhu'];
+        $hasil;
+    }
     ?>
     <form action="" method="post">
-        Nilai : <input type="text" name="nilai" value="<?php echo $suhu ?>">
+        Nilai : <input type="text" name="nilai" value="<?php if (isset($suhu)) echo $suhu ?>">
         Dari: <br>
         <input type="radio" name="dariSuhu" <?php if (isset($dariSuhu) && $dariSuhu == "Celcius") echo "checked"; ?> value="Celcius"> Celcius <br>
         <input type="radio" name="dariSuhu" <?php if (isset($dariSuhu) && $dariSuhu == "Fahrenheit") echo "checked"; ?> value="Fahrenheit"> Fahrenheit <br>
@@ -28,94 +31,95 @@
         <input type="radio" name="keSuhu" <?php if (isset($keSuhu) && $keSuhu == "Fahrenheit") echo "checked"; ?> value="Fahrenheit"> Fahrenheit <br>
         <input type="radio" name="keSuhu" <?php if (isset($keSuhu) && $keSuhu == "Rheamur") echo "checked"; ?> value="Rheamur"> Rheamur <br>
         <input type="radio" name="keSuhu" <?php if (isset($keSuhu) && $keSuhu == "Kelvin") echo "checked"; ?> value="Kelvin"> Kelvin <br>
-        <input type="submit" value="Konversi">
+        <input type="submit" name="enter" value="Konversi">
     </form>
     <?php
-
-    switch ($dariSuhu) {
-        case "Celcius":
-            switch ($keSuhu) {
-                case 'Celcius':
-                    $hasil = $suhu;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
-                    break;
-                case 'Fahrenheit':
-                    $hasil = $suhu * 1.8 + 32;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°F</h2>";
-                    break;
-                case "Rheamur":
-                    $hasil = $suhu * 0.8;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
-                    break;
-                case 'Kelvin':
-                    $hasil = $suhu + 273;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
-                    break;
-            }
-            break;
-        case "Fahrenheit":
-            switch ($keSuhu) {
-                case 'Celcius':
-                    $hasil = ($suhu - 32) * 5 / 9;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
-                    break;
-                case 'Fahrenheit':
-                    $hasil = $suhu;
-                    echo "<h2> Hasil Konversi:" . $hasil . "℉</h2>";
-                    break;
-                case "Rheamur":
-                    $hasil = ($suhu - 32) * 0.44444;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
-                    break;
-                case 'Kelvin':
-                    $hasil = ($suhu - 32) * 5 / 9 + 273;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
-                    break;
-            }
-            break;
-        case "Rheamur":
-            switch ($keSuhu) {
-                case 'Celcius':
-                    $hasil = $suhu  / 0.8;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
-                    break;
-                case 'Fahrenheit':
-                    $hasil = $suhu * 2.25 + 32;
-                    echo "<h2> Hasil Konversi:" . $hasil . "℉</h2>";
-                    break;
-                case "Rheamur":
-                    $hasil = $suhu;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
-                    break;
-                case 'Kelvin':
-                    $hasil = ($suhu / 0.8) + 273;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
-                    break;
-            }
-            break;
-        case "Kelvin":
-            switch ($keSuhu) {
-                case 'Celcius':
-                    $hasil = $suhu - 273;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
-                    break;
-                case 'Fahrenheit':
-                    $hasil = ($suhu - 273) * 9 / 5 + 32;
-                    echo "<h2> Hasil Konversi:" . $hasil . "℉</h2>";
-                    break;
-                case "Rheamur":
-                    $hasil = ($suhu - 273) * 4 / 5;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
-                    break;
-                case 'Kelvin':
-                    $hasil = $suhu;
-                    echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
-                    break;
-            }
-            break;
-        default:
-            echo "inputan salah";
-            break;
+    if (isset($_POST['enter'])) {
+        switch ($dariSuhu) {
+            case "Celcius":
+                switch ($keSuhu) {
+                    case 'Celcius':
+                        $hasil = $suhu;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
+                        break;
+                    case 'Fahrenheit':
+                        $hasil = $suhu * 1.8 + 32;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°F</h2>";
+                        break;
+                    case "Rheamur":
+                        $hasil = $suhu * 0.8;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
+                        break;
+                    case 'Kelvin':
+                        $hasil = $suhu + 273;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
+                        break;
+                }
+                break;
+            case "Fahrenheit":
+                switch ($keSuhu) {
+                    case 'Celcius':
+                        $hasil = ($suhu - 32) * 5 / 9;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
+                        break;
+                    case 'Fahrenheit':
+                        $hasil = $suhu;
+                        echo "<h2> Hasil Konversi:" . $hasil . "℉</h2>";
+                        break;
+                    case "Rheamur":
+                        $hasil = ($suhu - 32) * 0.44444;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
+                        break;
+                    case 'Kelvin':
+                        $hasil = ($suhu - 32) * 5 / 9 + 273;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
+                        break;
+                }
+                break;
+            case "Rheamur":
+                switch ($keSuhu) {
+                    case 'Celcius':
+                        $hasil = $suhu  / 0.8;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
+                        break;
+                    case 'Fahrenheit':
+                        $hasil = $suhu * 2.25 + 32;
+                        echo "<h2> Hasil Konversi:" . $hasil . "℉</h2>";
+                        break;
+                    case "Rheamur":
+                        $hasil = $suhu;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
+                        break;
+                    case 'Kelvin':
+                        $hasil = ($suhu / 0.8) + 273;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
+                        break;
+                }
+                break;
+            case "Kelvin":
+                switch ($keSuhu) {
+                    case 'Celcius':
+                        $hasil = $suhu - 273;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°C</h2>";
+                        break;
+                    case 'Fahrenheit':
+                        $hasil = ($suhu - 273) * 9 / 5 + 32;
+                        echo "<h2> Hasil Konversi:" . $hasil . "℉</h2>";
+                        break;
+                    case "Rheamur":
+                        $hasil = ($suhu - 273) * 4 / 5;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°R</h2>";
+                        break;
+                    case 'Kelvin':
+                        $hasil = $suhu;
+                        echo "<h2> Hasil Konversi:" . $hasil . "°K</h2>";
+                        break;
+                }
+                break;
+            default:
+                echo "inputan salah";
+                break;
+        }
     }
     ?>
 </body>
