a3abcd916bfd8931c305fe24db42aa0a092c9bc7
uluumbch <uluum123@gmail.com>
2022-03-18 14:35:16 +0800

    mengganti algoritma sort

--- modul2/PRAK201.php
+++ modul2/PRAK201.php
@@ -9,18 +9,39 @@
 </head>
 
 <body>
+    <?php
+    function insertionSort($array)
+    {
+        for ($i = 0; $i < count($array); $i++) {
+            $val = $array[$i];
+            $j = $i - 1;
+            while ($j >= 0 && $array[$j] > $val) {
+                $array[$j + 1] = $array[$j];
+                $j--;
+            }
+            $array[$j + 1] = $val;
+        }
+        return $array;
+    }
+    ?>
     <form action="" method="post">
         Nama 1 : <input type="text" name="nama1"> <br>
         Nama 2 : <input type="text" name="nama2"><br>
         Nama 3 : <input type="text" name="nama3">
-        <input type="submit" value="urutkan">
+        <input type="submit" name="submit" value="urutkan">
     </form>
 
     <?php
-    asort($_POST);
-    foreach ($_POST as $value) {
-        echo $value;
-        echo "<br>";
+    // asort($_POST);
+    if (isset($_POST['submit'])) {
+        $hasil = array();
+        $hasil[] = $_POST['nama1'];
+        $hasil[] = $_POST['nama2'];
+        $hasil[] = $_POST['nama3'];
+        foreach (insertionSort($hasil) as $value) {
+            echo $value;
+            echo "<br>";
+        }
     }
     ?>
 </body>
--- modul2/PRAK203.php
+++ modul2/PRAK203.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Soal 3 - Konversi Suhu</title>
+    <title>Soal 3 - Bachrul Uluum</title>
 </head>
 
 <body>
