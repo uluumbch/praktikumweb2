66cdedd64a438e16c617b94babd807cb79311f13
uluumbch <uluum123@gmail.com>
2022-05-29 12:47:51 +0800

    memperbaiki Member.php

--- modul6/Member.php
+++ modul6/Member.php
@@ -1,4 +1,8 @@
 <?php
+session_start();
+if (!isset($_SESSION['nomor_member'])) {
+    header("Location: ErrorPage.php");
+}
 require('./Model.php');
 if (isset($_GET['id_member'])) {
     hapusmember($_GET['id_member']);
@@ -7,12 +11,14 @@ if (isset($_GET['id_member'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Member</title>
 </head>
+
 <body>
     <a href="FormMember.php"><button>Tambah Data</button></a>
     <table border="1">
@@ -27,4 +33,5 @@ if (isset($_GET['id_member'])) {
         <?= tampildata("member") ?>
     </table>
 </body>
+
 </html>
